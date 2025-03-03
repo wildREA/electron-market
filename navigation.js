29fb91b4cc5e31edffef3413467eaf5e7dfc892b
wildREA <wildrealinit@gmail.com>
2025-03-03 10:05:22 +0100

    Updating files

---
+++ navigation.js
@@ -0,0 +1,7 @@
+document.getElementById('option1').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+
+document.getElementById('option2').addEventListener('click', function() {
+    window.location.href = 'profile.html';
+});
