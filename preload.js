bf0d1f5a6adf0ce66bd0716a62de1ce61a09ad74
Mech654 <drahmet2007@hotmail.com>
2025-03-04 10:58:30 +0100

    1. Configured nodemon. 2. Navigation bar. 3. Some code improvements

--- preload.js
+++ preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
   send: (channel, data) => ipcRenderer.send(channel, data),
   receive: (channel, callback) => ipcRenderer.on(channel, (_, ...args) => callback(...args)),
-  ye: () => ipcRenderer.invoke("ye"),
+  profile: () => ipcRenderer.invoke("profile"),
+  market: () => ipcRenderer.invoke("market"),
 });
