90f006169f97c28213dd8d880596cb2733c49222
wildREA <wildrealinit@gmail.com>
2025-03-05 12:29:10 +0100

    Updating files

--- app/navigation.js
+++ app/navigation.js
@@ -1,8 +1,9 @@
+import { renderCars } from './profile.js'
+
 document.getElementById("market").addEventListener("click", async () => {
     const response = await window.electronAPI.market();
 });
 
 document.getElementById("profile").addEventListener("click", async () => {
-    const response = await window.electronAPI.profile();
+    renderCars();
 });
-
