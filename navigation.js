762d5d9890ec72d4f60efc0963e35658bd5d7d1d
wildREA <wildrealinit@gmail.com>
2025-03-05 12:05:03 +0100

    Updating files

--- navigation.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("market").addEventListener("click", async () => {
-    const response = await window.electronAPI.market();
-});
-
-document.getElementById("profile").addEventListener("click", async () => {
-    const response = await window.electronAPI.profile();
-});
-
--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  send: (channel, data) => ipcRenderer.send(channel, data),
-  receive: (channel, callback) => ipcRenderer.on(channel, (_, ...args) => callback(...args)),
-  profile: () => ipcRenderer.invoke("profile"),
-  market: () => ipcRenderer.invoke("market"),
-});
