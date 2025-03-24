9a7d5ec292e23c894655923ddd8fb0adb8e0cc62
wildREA <wildrealinit@gmail.com>
2025-03-24 12:54:40 +0100

    Updating files

--- app/renderer.js
+++ app/renderer.js
@@ -1,22 +1,3 @@
-const socket = new WebSocket('ws://localhost:3000');
-
-socket.onopen = () => {
-  console.log('Connected to the WebSocket server');
-  socket.send('Hello from Electron!');
-}
-
-socket.onmessage = (event) => {
-  console.log('Message from server:', event.data);
-}
-
-socket.onclose = () => {
-  console.log('Disconnected from the WebSocket server');
-}
-
-socket.onerror = (err) => {
-  console.log('WebSocket error:', err.message);
-}
-
 // Sending data to the main process:
 window.api.send('toMain', { message: 'Hello from the API, main!' });
 
