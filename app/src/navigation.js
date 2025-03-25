22e0a259c1a9547dd7a9a93005f6cdfe3461b78f
Wild <wildrealinit@gmail.com>
2025-03-25 14:18:09 +0100

    Updating files

--- app/src/navigation.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("profile").addEventListener("click", () => {
-    buildProfileCard();
-});
-
-document.getElementById("account").addEventListener("click", () => {
-    createLogin();
-});
