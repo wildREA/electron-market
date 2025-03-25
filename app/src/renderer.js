78e5d9f4a97f10ea825613be8037d25860e574a8
Wild <wildrealinit@gmail.com>
2025-03-25 14:18:09 +0100

    Updating files

--- app/src/renderer.js
+++
@@ -1,7 +0,0 @@
-// Sending data to the main process:
-window.api.send('toMain', { message: 'Hello from the API, main!' });
-
-// Receiving data from the main process:
-window.api.receive('fromMain', (data) => {
-  console.log('Received from main:', data);
-});
